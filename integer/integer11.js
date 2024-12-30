0d7636de18b8640544e9927489f89ee2176fbee1
Baxtiyor Shamshiddinov <baxtiyor.shamshiddinov@mail.ru>
2024-12-30 21:23:43 +0500

    intial commit

---
+++ integer/integer11.js
@@ -0,0 +1,12 @@
+let a = 10;
+
+let b = Math.floor(a/10); // b- o'nlik
+let s = a % 10; // s- birlik
+
+let yigindi = b + s; 
+let kopaytma =b * s;
+
+console.log("Yig'indisi: " + yigindi); // Yig'indisi: 5
+console.log("Ko'paytmasi: " + kopaytma); // Ko'paytmasi: 6
+
+
