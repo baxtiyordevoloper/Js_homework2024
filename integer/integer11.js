35923fba6b79e8f2d847f4cfe586bc265a826326
Baxtiyor Shamshiddinov <baxtiyor.shamshiddinov@mail.ru>
2024-12-31 10:52:21 +0500

    integer12 added0

--- integer/integer11.js
+++ integer/integer11.js
@@ -1,12 +1,15 @@
-let a = 10;
+let a = 23;
 
-let b = Math.floor(a/10); // b- o'nlik
-let s = a % 10; // s- birlik
+let b = Math.floor(a/10); //2
+let s = a % 10 ; // 3
 
-let yigindi = b + s; 
-let kopaytma =b * s;
+let yigindi = b + s ;
+let kopaytma = b * s
 
-console.log("Yig'indisi: " + yigindi); // Yig'indisi: 5
-console.log("Ko'paytmasi: " + kopaytma); // Ko'paytmasi: 6
+console.log(yigindi);
+console.log(kopaytma);
+
+
+
 
 
---
+++ integer/integer12.js
@@ -0,0 +1,6 @@
+let n = 12;
+
+// let a = 2 * Math.pow(n+3,2); // 1 usul
+let a = 2 *(n+3)**2; // 2 usul
+
+console.log(a); // 450
