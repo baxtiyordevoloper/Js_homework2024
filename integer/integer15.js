ea4a2803a7e1f4b9b984c4b174d3f483345f3d51
Baxtiyor Shamshiddinov <baxtiyor.shamshiddinov@mail.ru>
2024-12-31 14:52:55 +0500

    integer 16 added

--- integer/integer15.js
+++ integer/integer15.js
@@ -1,3 +1,3 @@
-let a = -56;
-let b = Math.abs(-56);
+let a = -45;
+let b = Math.abs(-45);
 console.log(b);// 8
---
+++ integer/integer16.js
@@ -0,0 +1,4 @@
+let a = 5.678;
+// let m = Math.round(a*100) /100; 1 usul
+
+console.log(a.toFixed(2)); //5.68
